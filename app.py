e2ede81aa9c053d7722819f3281e821ce62ed3b1
Prarthan <prarthantalwar@gmail.com>
2023-08-09 21:19:48 +0530

    Created Flask instance

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template,redirect
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+     return render_template('home.html')
+
+@app.route('/<string:page_name>')
+def page(page_name="/"):
+     try:
+         return render_template(page_name+".html")
+     except:
+         return redirect("/")
+
+
+if __name__ == "__main__":
+  app.run(host='0.0.0.0', debug=True)
