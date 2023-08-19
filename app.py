94489f4b8d37a860f1fb939fded30751a9483e19
Prarthan <prarthantalwar@gmail.com>
2023-08-20 05:26:58 +0530

    Implemented half login

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
-from flask import Flask, render_template,redirect
+from flask import Flask, render_template,redirect, request
+from database import mycursor
+
+# COMMAND TO USE THE CREATED DATABASE
+mycursor.execute("USE talwar_supermarket_management_system")
 
 app = Flask(__name__)
 
@@ -11,9 +15,34 @@ def landing():
 @app.route('/login',methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        # handle request
-         pass
+        username = request.form['username']
+        password = request.form['password']
+        print(type(username), username, type(password), password)
+
+        query= f"SELECT EXISTS(SELECT * from USERS WHERE USERNAME='{username}' AND PASS='{password}')"
+        mycursor.execute(query)
+        data = mycursor.fetchall()
+        response = data[0][0]
+        if response == 1:
+            query=f"SELECT USER_ROLE FROM USERS WHERE USERNAME='{username}'"
+            print(query)
+            mycursor.execute(query)
+            data = mycursor.fetchall()
+            print(data,type(data))
+            response = data[0][0]
+            print(response, type(response))
+            if response == 'Administrator':
+                return redirect("/admin")
+            elif response == 'Data Operator':
+                return redirect("/dataop")
+            else:
+                return redirect("/billop")
     return render_template('login.html')
+
+
+@app.route("/logout")
+def logout():
+     return render_template('logout.html')
 
 
 
