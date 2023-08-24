babb81eba8c14c9d1e236e45dd49f1fa16a7f7f7
Prarthan <prarthantalwar@gmail.com>
2023-08-24 16:24:37 +0530

    Added all html

--- app.py
+++ app.py
@@ -77,10 +77,122 @@ def dataop():
 
 
 
+
+# Modules 
+
+@app.route('/user_deats')
+def user_deats():
+    if g.user:
+        return render_template('user_deats.html', user=session['user'])
+    return redirect('/')
+
+
+@app.route('/cust_deats')
+def cust_deats():
+    if g.user:
+        return render_template('cust_deats.html', user=session['user'])
+    return redirect('/')
+
+
+@app.route('/todays_trans')
+def todays_trans():
+    if g.user:
+        return render_template('todays_trans.html', user=session['user'])
+    return redirect('/')
+
+
+@app.route('/prod_supp_tod')
+def prod_supp_tod():
+    if g.user:
+        return render_template('prod_supp_tod.html', user=session['user'])
+    return redirect('/')
+
+
+@app.route('/supp_deats')
+def supp_deats():
+    if g.user:
+        return render_template('supp_deats.html', user=session['user'])
+    return redirect('/')
+
+
+@app.route('/search_prod_supp_deats')
+def search_prod_supp_deats():
+    if g.user:
+        return render_template('search_prod_supp_deats.html', user=session['user'])
+    return redirect('/')
+
+
+@app.route('/del_prod')
+def del_prod():
+    if g.user:
+        return render_template('del_prod.html', user=session['user'])
+    return redirect('/')
+
+
+@app.route('/prod_req')
+def prod_req():
+    if g.user:
+        return render_template('prod_req.html', user=session['user'])
+    return redirect('/')
+
+
+@app.route('/prod_30_exp')
+def prod_30_exp():
+    if g.user:
+        return render_template('prod_30_exp.html', user=session['user'])
+    return redirect('/')
+
+
+@app.route('/edit_prod_deats')
+def edit_prod_deats():
+    if g.user:
+        return render_template('edit_prod_deats.html', user=session['user'])
+    return redirect('/')
+
+
+@app.route('/search_prod_deats')
+def search_prod_deats():
+    if g.user:
+        return render_template('search_prod_deats.html', user=session['user'])
+    return redirect('/')
+
+
+@app.route('/list_prods')
+def list_prods():
+    if g.user:
+        return render_template('list_prods.html', user=session['user'])
+    return redirect('/')
+
+
+@app.route('/add_prods')
+def add_prods():
+    if g.user:
+        return render_template('add_prods.html', user=session['user'])
+    return redirect('/')
+
+
+@app.route('/apply_rem_disc')
+def apply_rem_disc():
+    if g.user:
+        return render_template('apply_rem_disc.html', user=session['user'])
+    return redirect('/')
+
+
+@app.route('/bill')
+def bill():
+    if g.user:
+        return render_template('bill.html', user=session['user'])
+    return redirect('/')
+
+
+# @app.errorhandler(401)  # Error handler for unauthorized access to the page (login required).
+
+
 @app.route("/logout")
 def logout():
      session.pop('user', None)
      return render_template('logout.html')
+
 
 @app.before_request
 def before_request():
