5341e3524e4f61b7a8d2fd2a25dbe450cbcaccea
Prarthan <prarthantalwar@gmail.com>
2025-08-16 13:25:30 +0530

    Database update to aiven from planetscale

--- database.py
+++ database.py
@@ -13,7 +13,7 @@ mycursor=myconn.cursor()
 #mycursor.execute("CREATE DATABASE BILLING_SYSTEM")
 
 # COMMAND TO USE THE CREATED DATABASE
-mycursor.execute("USE talwar_supermarket_management_system")
+mycursor.execute("USE super_market_management")
 
 #mycursor.execute("SELECT EXISTS(SELECT * from USERS WHERE USERNAME='admin' AND PASS='administrat' )")
 # mycursor.execute("SELECT USER_ROLE FROM USERS")
